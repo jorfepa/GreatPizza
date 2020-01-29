77881cad47c49fdba6359e0afb885386cd4cd340
jorfepa <jorfepa@gmail.com>
2020-01-29 11:58:23 -0400

    Add some comments in the code

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -1,13 +1,16 @@
 import { MongoClient } from "mongodb";
 
-export async function connect() {
+// This method is going to conect the client to the database
+export async function connect() {    
     try {
     const client = await MongoClient.connect('mongodb://localhost', {
         useUnifiedTopology: true});
     const db = client.db('great-pizza');
     return db;
-    }catch(err){
-        console.log(err);
+    }
+    // This cath is going to show an error message for the client
+    catch(err){
+        console.error(err);
     }
 }
 
